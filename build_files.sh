ba571f2a2a939c14c18e73e725a96adf6cc14ea5
jeongsooh <jeongsooh@hotmail.com>
2026-02-26 21:14:28 +0900

    Add --break-system-packages for Vercel pip install

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 # build_files.sh
-pip3 install urllib3==1.26.15
-pip3 install -r requirements.txt
+pip3 install urllib3==1.26.15 --break-system-packages
+pip3 install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
